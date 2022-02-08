30edbd711a68cb8ad19042ce9aa6af9261f99ebb
Josep Ll. Gelpi <gelpi@ub.edu>
2022-02-08 12:05:05 +0100

    Added REST API
    Former-commit-id: 374ffe80d3022964f763d2ec9bb1a228905eb82a
    Former-commit-id: 57adbf10dcb337ee4527f7947c52a1aa16467161

--- globals.inc.php
+++ globals.inc.php
@@ -6,15 +6,15 @@
 // Base directories
 // Automatic, taken from CGI variables.
 $baseDir = dirname($_SERVER['SCRIPT_FILENAME']);
-#$baseDir = '/home/dbw00/public_html/PDBBrowser';
+$baseDir = '/home/gelpi/DEVEL/WWW/DBW/PDBBrowser';
 $baseURL = dirname($_SERVER['SCRIPT_NAME']);
 
 // Temporal dir, create if not exists, however Web server 
 // may not have the appropriate permission to do so
 $tmpDir = "$baseDir/tmp";
-if (!file_exists($tmpDir)) {
-    mkdir($tmpDir);
-}
+// if (!file_exists($tmpDir)) {
+//     mkdir($tmpDir);
+// }
 // Blast details, change to adapt to local settings
 // Blast databases should be created using the appropriate programs.
 $blastHome = "/home/dbw00/blast";
@@ -36,18 +36,18 @@ $textFields = ['e.header', 'e.compound', 'a.author', 's.source', 'sq.header'];
 
 // Compounds
 $rs = mysqli_query($mysqli, "SELECT * from comptype") or print mysqli_error($mysqli);
-while ($rsF = mysqli_fetch_array($rs)) {
+while ($rsF = mysqli_fetch_assoc($rs)) {
     $compTypeArray[$rsF['idCompType']] = $rsF['type'];
 }
 
 //expTypes
 $rs = mysqli_query($mysqli,"SELECT * from expType") or print mysqli_error($mysqli);
-while ($rsF = mysqli_fetch_array($rs)) {
+while ($rsF = mysqli_fetch_assoc($rs)) {
     $expTypeArray[$rsF['idExpType']] = $rsF;
 }
 //expClasses
 $rs = mysqli_query($mysqli,"SELECT * from expClasse") or print mysqli_error($mysqli);
-while ($rsF = mysqli_fetch_array($rs)) {
+while ($rsF = mysqli_fetch_assoc($rs)) {
     $expClasseArray[$rsF['idExpClasse']] = $rsF['expClasse'];
 }
 // Start session to store queries
